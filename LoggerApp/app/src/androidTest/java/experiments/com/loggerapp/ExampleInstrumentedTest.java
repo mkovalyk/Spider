7b39163c947523279ac2f8bea0b75be28d92a81e
Mykhailo Kovalyk <mishakovalyk@gmail.com>
2017-10-27 00:37:35 +0300

    Move tests to another package.

--- LoggerApp/app/src/androidTest/java/experiments/com/loggerapp/ExampleInstrumentedTest.java
+++ LoggerApp/app/src/androidTest/java/experiments/com/loggerapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("experiments.com.loggerapp", appContext.getPackageName());
+        Assert.assertEquals("experiments.com.loggerapp", appContext.getPackageName());
     }
 }
